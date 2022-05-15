eeaed1ff94e377449a0f106b1b1b40c3c326d094
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-05-15 01:49:08 -0700

    update:ex-02
    in this we have completed ex-02 of the .

--- app.js
+++ app.js
@@ -4,13 +4,38 @@ const luckynumber=document.querySelector("#lucky-number")
 
 const button=document.querySelector("#btncheck")
 
+const output=document.querySelector("#output-value")
+
+
+
+// using modulo operator and using 
+
+function todivide(sum,luckynumber){
+    if(sum%luckynumber===0){
+        output.innerText="Your Birthday is Lucky"
+    }
+    else{
+        output.innerText="Your Birthday is Not Lucky,so sad"
+    }
+}
+
+
+
+
+
+
+
+
+
 // now taking input from the user
 function checkisbirthdaylucky(){
     const dob=dateofbirth.value;
     // console.log(dob);
 
     const sum=calculatesum(dob);
-    console.log(sum);
+    // console.log(sum);
+    todivide(sum,luckynumber.value);
+
 
 
 
